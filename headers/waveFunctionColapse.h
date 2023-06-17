87ce9b03228acae3d36c62a3cb7b3d6cb084754a
Jonathas dos Santos <jonathasdossantos007@gmail.com>
2023-06-17 13:49:21 -0300

    Funções de WFC adicionadas

--- headers/waveFunctionColapse.h
+++ headers/waveFunctionColapse.h
@@ -1,5 +1,13 @@
+#include <stdlib.h>
+
 #include <SDL.h>
 #include <SDL_image.h>
+
+// Representa posições em matrizes
+typedef struct pos{
+    int l;
+    int c;
+}pos;
 
 // Estrutura que representa um tile. Tem como campos uma textura de tile, seu id
 // e listas para todos os tiles aceitos para cada lado
@@ -12,8 +20,34 @@ typedef struct tile{
     SDL_Texture *tex;
 }tile;
 
+// Estrutura que representa uma posição na matriz do algoritmo WFC.
+// Ela contém todas as possibilidades de tile para esta posição quando não colapsado
+// e o tile escolhido após o colapso.
+typedef struct node{
+    int possibilities[80];
+    int entropy;
+    tile *t;
+}node;
+
 // Construtor para a struct tile
 tile *createTile(int id, int *t, int *l, int *b, int *r, char *imgPath, SDL_Renderer renderer);
 
 // Libera a memória alocada para uma struct tile
 void freeTile(tile *t);
+
+// Construtor para a struct node
+node *createNode();
+
+// Libera memória alocada para uma struct node
+void freeNode(node *n);
+
+// Quando o algoritmo adiciona um novo tile à matriz, a função atualiza 
+// os tiles possíveis (entropia) nos nodes ao redor
+void updateEntropy(node ***nodeMatrix, int w, int h, int x, int y);
+
+// Encontra a menor entropia em uma matriz e retorna a posição de um dos
+// nodes com esta entropia
+pos findLowestEntropy(node ***nodeMatrix, int w, int h);
+
+// Colapsa o node para a entropia mínima e atribui um tile possível para o node
+void collapseNode(node ***nodeMatrix, pos pos);
--- waveFunctionColapse.c
+++ waveFunctionColapse.c
@@ -42,3 +42,80 @@ void freeTile(tile *t) {
     SDL_DestroyTexture(t->tex);
     free(t);
 }
+
+node *createNode() {
+
+    node *n = malloc(sizeof(node));
+    for(int c=0; c < 80; c++) {
+        (n->possibilities)[c] = 1;
+    }
+
+    n->entropy = 80;
+    n->t = NULL;
+}
+
+void freeNode(node *n) {
+
+    free(n);
+}
+
+void updateEntropy(node ***nodeMatrix, int w, int h, int x, int y) {
+    int c;
+
+    // Confere node de cima
+    if(y > 0 && !nodeMatrix[y-1][x]->tile) {
+        for(c=0; c< nodeMatrix[y-1][x]->entropy; c++) {
+            ((nodeMatrix[y-1][x])->possibilities)[c] &= ((nodeMatrix[y][x])->t->t)[c];
+        }
+    }
+
+    // Confere node da esquerda
+    if(x > 0 && !nodeMatrix[y][x-1]->t) {
+        for(c=0; c< nodeMatrix[y][x-1]->entropy; c++) {
+            ((nodeMatrix[y][x-1])->possibilities)[c] &= ((nodeMatrix[y][x])->t->l)[c];
+        }
+    }
+
+    // Confere node de baixo
+    if(y<h && !nodeMatrix[y+1][x]->tile) {
+        for(c=0; c< nodeMatrix[y+1][x]->entropy; c++) {
+            ((nodeMatrix[y+1][x])->possibilities)[c] &= ((nodeMatrix[y][x])->t->b)[c];
+        }
+    }
+
+    // Confere node da direita
+    if(x<w && !nodeMatrix[y][x+1]->tile) {
+        for(c=0; c< nodeMatrix[y][x+1]->entropy; c++) {
+            ((nodeMatrix[y][x+1])->possibilities)[c] &= ((nodeMatrix[y][x])->t->r)[c];
+        }
+    }
+}
+
+pos findLowestEntropy(node ***nodeMatrix, int w, int h) {
+    int c;
+    int minEntropy = 80;
+    pos posi = {0,0};
+
+    for(c=0; c<w; c++) {
+        for(i=0; i<h, i++) {
+            if((nodeMatrix[c][i])->tile) continue;
+            if(minEntropy >= (nodeMatrix[c][i])->entropy) {
+                minEntropy = nodeMatrix[c][i])->entropy;
+                pos.l = c;
+                pos.c = i;
+            }
+        }
+    }
+
+    return posi;
+}
+
+void collapseNode(node ***nodeMatrix, pos pos) {
+
+    node *n = nodeMatrix[pos.l][pos.c];
+
+    int id = (n->possibilities)[rand() % 80];
+
+    n->entropy = 1;
+    n->t = NULL; // Tile com id 1
+}
